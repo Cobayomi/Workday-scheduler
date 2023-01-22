5cde21207ca51acb057fc8477f23354f2c54f180
capricia <capriciaw16@gmail.com>
2023-01-22 02:07:39 -0600

    new js

--- script.js
+++ script.js
@@ -2,47 +2,61 @@
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
-var timeDisplay = $('#currentDay');
-var btnEl = $('button');
-var textEl = $('textarea');
+//found a way to display date here https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
+document.getElementById("currentDay").innerHTML = today;
 
 
-var day = moment().format('dddd, MMMM Do');
-timeDisplay.text(day);
-
-
-textEl.each(function(){
-    var rowTime = $(this).attr('id');
-    console.log(rowTime)
-    var currentTime = moment().hour();
-    console.log(currentTime)
-    
-    if (rowTime < currentTime){
-        $(this).addClass('past');
-    }else if (rowTime > currentTime){
-        $(this).addClass('future')
-    }else{
-         $(this).addClass('present')
-       
-    }
-})
-
-    btnEl.on("click",function(){
-        var value = ($(this).parent().siblings('textarea').val())
-        var idEl = ($(this).parent().siblings('textarea').attr('id'))
-        localStorage.setItem(idEl, value)
+$(document).ready(function() {
+    $(".saveBtn").on("click", function() {
+       var value = $(this).siblings('.description').val();
+       var time = $(this).parent().attr('id');
+       localStorage.setItem(time, value);
+$('.notification').addClass('show')
+ 
+setTimeout(function() {
+    $('.notification').removeClass('show');
     });
 
-    function getTime(){
-        var input = localStorage.getItem('9')
-        $('#9').text(input)
-        $('#10').text(localStorage.getItem('10'))
-        $('#11').text(localStorage.getItem('11'))
-        $('#12').text(localStorage.getItem('12'))
-        $('#1').text(localStorage.getItem('1'))
-        $('#2').text(localStorage.getItem('2'))
-        $('#3').text(localStorage.getItem('3'))
-        $('#4').text(localStorage.getItem('4'))
-        $('#5').text(localStorage.getItem('5'))
-    }
-getTime()
+   })
+
+   function hourNow() {
+       var currentTime = moment().hours();
+    
+        $(".time-block").each(function() {
+        var blockHour= parseInt($(this).attr('id').split('-')[1]);
+           if(blockHour < currentTime) {
+               $(this).addClass("past");
+           } else if(blockHour === currentTime) {
+               $(this).removeClass("past");
+               $(this).addClass("present")
+           } else{
+               $(this).removeClass("past");
+               $(this).removeClass("present");
+               $(this).addClass("future");
+           }
+   
+       })
+   }
+  
+   hourNow()
+   var interval = setInterval(hourNow)
+
+   $('#hour-1 .description').val(localStorage.getItem('hour-1'))
+   $('#hour-2 .description').val(localStorage.getItem('hour-2'))
+   $('#hour-3 .description').val(localStorage.getItem('hour-3'))
+   $('#hour-4 .description').val(localStorage.getItem('hour-4'))
+   $('#hour-5 .description').val(localStorage.getItem('hour-5'))
+   $('#hour-6 .description').val(localStorage.getItem('hour-6')) 
+   $('#hour-7 .description').val(localStorage.getItem('hour-7'))
+   $('#hour-8 .description').val(localStorage.getItem('hour-8'))
+   $('#hour-9 .description').val(localStorage.getItem('hour-9'))
+   $('#hour-10 .description').val(localStorage.getItem('hour-10'))
+
+});
